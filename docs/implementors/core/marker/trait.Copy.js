ec998700cbf3f18162c7f6f10cc18267fe713e8e
Kieranoski702 <Kieranoski702@users.noreply.github.com>
2023-11-22 15:41:42 +0000

    Actions: Documentation for f7523c1e599a239935fb0a7f65dac01a90d2db24

--- docs/implementors/core/marker/trait.Copy.js
+++ docs/implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"minion_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"minion_rs/ast/enum.VarDomain.html\" title=\"enum minion_rs::ast::VarDomain\">VarDomain</a>"]]
+"minion_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"minion_rs/ast/enum.VarDomain.html\" title=\"enum minion_rs::ast::VarDomain\">VarDomain</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
